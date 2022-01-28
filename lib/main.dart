3fe6ac39b0f6f80ce9adb64be7cff3b5e6a3a315
archi <archigini@gmail.com>
2022-01-29 00:30:51 +0500

    Виджет состояния_AppBar_Navigation_Drawer_Divider

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:app_bar_demo/my_navigation_drawer_screen.dart';
+import 'package:app_bar_demo/panel_app_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'switches_demo_screen.dart';
+
+void main () {
+        runApp (const NavigationDrawerScreen());
+}
+
+
+
+
